a5a2c4dd1ae124003d1ab0813141f16488043c98
iraklyoda <iraklyoda42@gmail.com>
2022-10-08 18:44:09 +0400

    please stop functionality

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+let count = 0;
+
+const countElement = document.querySelector('#count');
 const before = document.querySelector('#one')
 const after = document.querySelector('#two');
 
 before.addEventListener('click', () => {
+  count++;
   before.classList.add('none');
   after.classList.remove('none');
+  countElement.textContent = count;
+  check(count);
 });
 
 after.addEventListener('click', () => {
+  count++;
   after.classList.add('none');
   before.classList.remove('none');
+  countElement.textContent = count;
+  check(count);
 });
+
+function check(count) {
+  if (count >= 100) {
+    countElement.textContent = 'კაი გეყოს';
+    before.classList.add('none');
+    after.classList.remove('none');
+  }
+}
+
